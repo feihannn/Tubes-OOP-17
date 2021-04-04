a6c515657b8bad8eaa41707650268654dafeb341
288-Farhan <farhansimatupang@gmail.com>
2021-04-04 12:50:29 +0700

    ngubah dikit
    ngubah Player.java dikit sama nambahin DeclareHIji

--- Player/Player.java
+++ Player/Player.java
@@ -1,90 +1,36 @@
 import java.util.ArrayList;
+import Card.Deck.java;
+
 public class Player {
     private String playerName;
     private int playerTurn;
-    public ArrayList<Card> kartu;//Ini kartu tangan
-    public ArrayList<Card> discardable_kartu;//Ini kartu tangan yang bisa dibuang nantinya
+    private ArrayList<Card> kartu;
     static int numofPlayer = 0;
-    private Deck dek_kartu= new Deck();//Buat tumpukan kartu semuanya(fungsinya biar nanti pas draw tinggal ngerandom dari ini)
+    Deck deck = new Deck();
 
     //Constructor Player
     public Player(String playerName, int playerTurn) {
         this.playerName = playerName;
         this.playerTurn = playerTurn;
         kartu = new ArrayList<Card>();
-        discardable_kartu = new ArrayList<Card>();
         numofPlayer ++;
     }
     // Bikin kartu yang dipegang sama player
-    public void initialHand(){
+    public void takeDeck(){
         for (int i = 0; i < 7; i++){
-            kartu.add(dek_kartu.getCard()); //ini fungsi drawRandomCard belum dibikin
+            kartu.add(deck.getCard()); //ini fungsi tlg di check lagi soalnya list yang tipe Card sedangkan ini tipenya String
         }
     }
-    //Fungsi F04
-    public void draw(){
-        kartu.add(dek_kartu.getCard());
-    }
-    //Perantara buat fungsi F05
-    public void hasOneCard(){
+    public boolean hasOneCard(){
         if (kartu.size() == 1){
-            //declareHiji();
+            declareHiji();
         }
     }
-    //Fungsi F02
     public void getHand(){
-        System.out.println("List Kartu Tangan: ");
         for (int i = 0; i < kartu.size(); i++){
-            System.out.println((i+1)+". "+kartu.get(i));
+            System.out.println(kartu.get(i));
         }
     }
-
-    //Menampilkan kartu yang bisa di discard
-    public void getdiscardableHand(Card top_card){
-        System.out.println("Kartu yang bisa dikeluarkan pemain: ");
-        this.discardable_kartu.clear();
-        for (int i = 0; i < kartu.size(); i++){
-            if(discardable(kartu.get(i), top_card)){
-                this.discardable_kartu.add(kartu.get(i));
-            }
-        }
-        for (int i = 0; i < discardable_kartu.size(); i++){
-            System.out.println((i+1)+". "+discardable_kartu.get(i));
-        }
-
-    }
-    //Buang kartu
-    public void discard(int i){
-        discardable_kartu.remove(i);
-    }
-    //Fungsi mengecheck apakah kartu bisa di discard
-    public boolean discardable(Card kartu_1, Card kartu_2){//Asumsi kartu 2 selalu kartu yang paling atas
-        return isMatchActionCard(kartu_1, kartu_2)||isMatchNumberCard(kartu_1, kartu_2)||isSameColor(kartu_1, kartu_2)||isValidWildCard(kartu_1);
-    }
-    
-    //Fungsi mengecheck apakah kartu paling atas sama dengan kartu yang ada di tangan
-    public static boolean isMatchActionCard(Card topCard, Card playedCard) {
-        if(topCard.getType()!=null&&playedCard.getType()!=null){
-            return isSameColor(topCard, playedCard)||topCard.getType().toString().equals(playedCard.getType().toString());
-        }else{return false;}
-    }
-
-    public static boolean isMatchNumberCard(Card topCard, Card playedCard) {
-        if(topCard.getNum()!=null&&playedCard.getNum()!=null){
-            return isSameColor(topCard, playedCard)||topCard.getNum().toString().equals(playedCard.getNum().toString());
-        }else{
-            return false;
-        }
-    }
-
-    public static boolean isValidWildCard(Card playedCard) {
-        return playedCard.getColor().toString().equals(Color.BLACK.toString());
-    }
-
-    private static boolean isSameColor(Card topCard, Card playedCard) {
-        return topCard.getColor().toString().equals(playedCard.getColor());
-    }
-    //Getter
     public String getPlayerName(){
         return playerName;
     }
@@ -94,10 +40,5 @@ public class Player {
     public static int getnumPlayers(){
         return numofPlayer;
     }
-    public ArrayList<Card> getKartuTangan(){
-        return kartu;
-    }
-    public ArrayList<Card> getDiscardableKartuTangan(){
-        return discardable_kartu;
-    }
 }
+// Tambahin = 1. Has hand card(Panjang aray kartu 1 panggil declare hiji); 2.Getternya (List kartu hand, nama, urutan);
